d898020b8084a0e3ea5c5304a6ea5149b96e5c0e
Agnes <agnesoserayn@gmail.com>
2024-05-18 19:10:05 +0300

    making code more readable

--- Day 002/day-2-start.py
+++ Day 002/day-2-start.py
@@ -1,6 +1,7 @@
-print("\033c")
-two_digit_number = input()
-# Do not change the code above
-# Write your code below this line
+# User shoul input a two digit number e.g, 32
+# Output should be the sum of the number's digits e.g, 5
 
-print(int(two_digit_number[0]) + int(two_digit_number[1]))
+print("\033c")
+two_digit_number = input("Enter a two digit number")
+sum = int(two_digit_number[0]) + int(two_digit_number[1])
+print(sum)
