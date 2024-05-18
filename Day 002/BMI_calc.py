8040526d9a0650801bc2a48daf1b443608c92e37
Agnes <agnesoserayn@gmail.com>
2024-05-18 19:03:48 +0300

    cleaning up code

--- Day 002/BMI_calc.py
+++ Day 002/BMI_calc.py
@@ -1,11 +1,12 @@
-print("\033c")
+# Simple BMI calculator 
+# BMI = weight / (height * height)
 # 1st input; enter height in meters e.g:1.65
 # 2nd input; enter weight in kilograms e.g:72
-height = input()
-weight = input()
-# Do not change the code above
-# Write your code below this line
-# BMI = weight / (height * height)
+
+print("\033c")
+height = input("Enter height in meters")
+weight = input("Enter weight in kilograms")
+
 den = float(height) * float(height)
 num = int (weight)
 BMI = (num / den)
