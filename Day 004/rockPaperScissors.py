d78d90e6b1597ba9bd2df3dfbf61013efac624a6
Agnes <agnesoserayn@gmail.com>
2024-05-19 20:55:36 +0300

    final build

--- Day 004/rockPaperScissors.py
+++ Day 004/rockPaperScissors.py
@@ -1,3 +1,19 @@
+# Official rock, paper, scissors rules
+# Rock beats scissors
+# Scissors beats paper
+# Paper beats rock
+
+# Things to consider:
+# How to decide who won and who lost
+# How to get the computer to choose a random shape between rock, paper and scissors
+# How to make sure the game works as it should
+
+# Breaking down problem
+# 1. Generating random number between 1 and 3
+# 2. Putting down the logic on how to win
+# 3. Thony
+
+import random
 rock = '''
     _______
 ---'   ____)
@@ -24,3 +40,24 @@ scissors = '''
       (____)
 ---.__(___)
 '''
+game_images = [rock, paper, scissors]
+user_choice = int(input("What do you choose? Type 0 for rock, 1 for paper and 2 for scissors\n"))
+if user_choice >= 3 or user_choice < 0 :
+    print("You typed an invalid number, You lose!")
+else:
+    print(game_images[user_choice])
+
+    computer_choice = random.randint(0,2)
+    print("Computer chose:")
+    print(game_images[computer_choice])
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose!")
+    elif computer_choice > user_choice:
+        print("You lose!")
+    elif user_choice > computer_choice:
+        print("You win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
