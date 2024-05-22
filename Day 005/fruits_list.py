1d8f1e13aefdc90586d3536050d4220db1e91e28
Agnes <agnesoserayn@gmail.com>
2024-05-22 09:35:07 +0300

    conforming to pyliny

--- Day 005/fruits_list.py
+++ Day 005/fruits_list.py
@@ -1,8 +1,10 @@
 # Under the list fruits, there are 3 items
 # The for loops iterantes and prints every fruit under 'fruits list'
-# For the second print statement, it gets executed after the first print statement then adds 'Pie' to the fruit for each fruit
+# For the second print statement, it gets executed after the first print statement
+# Then adds 'Pie' to the fruit for each fruit
 # The last line of code is outside the loop
-# This means that it won't iterate through each item on the list but rather displays the list as a whole
+# This means that it won't iterate through each item on the list
+#  but rather displays the list as a whole
 
 fruits = ["Apple", "Peach", "Pearl"]
 for fruit in fruits:
