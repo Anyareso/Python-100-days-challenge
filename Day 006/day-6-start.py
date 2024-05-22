03a4a3e72f1543a37e6f7de6c64aa73592a8bace
Agnes <agnesoserayn@gmail.com>
2024-05-22 10:16:08 +0300

    defining and calling functions

---
+++ Day 006/day-6-start.py
@@ -0,0 +1,10 @@
+# Inorder to creat functions:
+# Step 1: Define function
+# Step 2: Write lines of code in the function
+# Step 3: Call the function
+# Here you specify the name and place ()
+def my_function():
+    print("Hello")
+    print("Bye")
+
+my_function()
