442e31d80a45a313bd46c37e9b63691c855b04c8
Agnes <agnesoserayn@gmail.com>
2024-05-27 14:55:59 +0300

    using functions with input

---
+++ Day 008/start.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+    print("Welcome ")
+    print("I hope you are well.")
+    print("Send money:)")
+
+greet()
+
+def greet_with_name(name, money):
+    print(f"Welcome {name}")
+    print(f"Send money:) {money}")
+
+greet_with_name("Agnes",20000)
