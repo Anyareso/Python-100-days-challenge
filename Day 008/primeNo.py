92e66f3a5f507a93f6199f9076552d41c3369e0c
Agnes <agnesoserayn@gmail.com>
2024-05-28 16:06:57 +0300

    solving final error

--- Day 008/primeNo.py
+++ Day 008/primeNo.py
@@ -1,15 +1,4 @@
 def prime_checker(number):
-    # if number == 1:
-    #     print(f"{number} is not a prime number")
-    # elif number > 1:
-    #     for i in range(2,number):
-    #         if (number % i) == 0:
-    #             print(f"{number} is not a prime number")
-    #             break
-    #     else:
-    #         print(f"{number} is a prime number")
-    # else:
-    #     print(f"{number}is not a prime number")
     isPrime = True
     for i in range(2, number):
         if number % i == 0:
@@ -20,5 +9,5 @@ def prime_checker(number):
         print("It is not a prime number")
 
 
-n = input("Enter a number you want to check")
+n = int(input("Enter a number you want to check"))
 prime_checker(number=n)
