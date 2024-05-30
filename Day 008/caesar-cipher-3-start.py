fb60ae54c969b1a9dead51b91b9262c95efd5f40
Agnes <agnesoserayn@gmail.com>
2024-05-30 09:25:12 +0300

    step 3 done

--- Day 008/caesar-cipher-3-start.py
+++ Day 008/caesar-cipher-3-start.py
@@ -14,6 +14,6 @@ def caesar(start_text, shift_amount, cipher_direction):
             new_position = position + shift_amount
             end_text += alphabet[new_position]
         print(f"Here's the {direction}d result: {end_text}")
-
+    
 #TODO-2: Call the caesar() function, passing over the 'text', 'shift' and 'direction' values.
 caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
