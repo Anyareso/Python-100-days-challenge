5a1a37b2d1a5ed6b0a2317d8dfc5ef595851a4c3
Agnes <agnesoserayn@gmail.com>
2024-06-24 15:50:15 +0300

    project part 2 completed

--- Day 017/quiz game/main.py
+++ Day 017/quiz game/main.py
@@ -1,12 +1,10 @@
-# TODO 1. Write a for loop to iterate over the question data.
-# TODO 2. Create a Question object from each entry in question_data
-# TODO 3. Append each Question object to the question_bank
 from question_model import Question
 from data import question_data
 
 question_bank = []
-
+# TODO 1. Write a for loop to iterate over the question data.
 for item in question_data:
+    # TODO 2. Create a Question object from each entry in question_data
     question = Question(item['text'], item['answer'])
+    # TODO 3. Append each Question object to the question_bank
     question_bank.append(question)
-  
