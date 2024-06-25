6e785fe749342e90428769fc65a6f27fe4e0df26
Agnes <agnesoserayn@gmail.com>
2024-06-25 12:10:59 +0300

    modifying main to accept the new question data

--- Day 017/quiz game/main.py
+++ Day 017/quiz game/main.py
@@ -6,12 +6,13 @@ question_bank = []
 # TODO 1. Write a for loop to iterate over the question data.
 for item in question_data:
     # TODO 2. Create a Question object from each entry in question_data
-    question = Question(item['text'], item['answer'])
+    question = Question(item['question'], item['correct_answer'])
     # TODO 3. Append each Question object to the question_bank
     question_bank.append(question)
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_questions():
     quiz.next_question()
-    print("You've completed the quiz")
-    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
+
+print("You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
