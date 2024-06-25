c2b6fd1e4dc58a3a6f1c1828f5276ca134adb1bd
Agnes <agnesoserayn@gmail.com>
2024-06-25 11:07:49 +0300

    project part 4 completed

--- Day 017/quiz game/quiz_brain.py
+++ Day 017/quiz game/quiz_brain.py
@@ -8,7 +8,10 @@
 # Create a method called next_question() inside QuizBrain
 # This method should retrieve the item at the current question_number from the question list.
 # Use the input() function to show the user the Question text and ask for the user's answer.
-
+# Challenge 3
+# Create a method called still_has_questions()
+# Return a boolean depending on the value of the question_number
+# Use the while loop to show the next question until the end
 # TODO 2. Checking if the answer was correct
 # TODO 3. Checking if we're at the end of the quiz
 
@@ -16,6 +19,11 @@ class QuizBrain:
     def __init__(self, question_list):
         self.question_number = 0
         self.question_list = question_list
+
+    def still_has_questions(self):
+        q_no = self.question_number
+        q_list = len(self.question_list)
+        return q_no < q_list
 
     def next_question(self):
         current_question = self.question_list[self.question_number]
