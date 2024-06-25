2b923d4584ee48dc032a9df6e511659ae3a8b6cf
Agnes <agnesoserayn@gmail.com>
2024-06-25 14:44:14 +0300

    creating polygons

--- Day 018/main.py
+++ Day 018/main.py
@@ -8,13 +8,49 @@ tim.color("green4")
 #     tim.forward(100)
 #     tim.right(90)
 
-for _ in range(15):
-    tim.forward(20)
-    tim.penup()
-    tim.forward(20)
-    tim.pendown()
+# for _ in range(15):
+#     tim.forward(20)
+#     tim.penup()
+#     tim.forward(20)
+#     tim.pendown()
 
 
+def square():
+    for _ in range(4):
+        tim.right(90)
+        tim.forward(100)
+
+
+def agons():
+    for _ in range(5):
+        tim.right(60)
+        tim.forward(100)
+
+def triangle(side_length):
+    for _ in range(3):
+        tim.right(120)
+        tim.forward(side_length)
+
+
+triangle(100)
+# square()
+# agons()
+
+class Shapes:
+    def __init__(self):
+        self.length = 0
+        self.sides = 0
+
+    def polygon(no_of_sides):
+        for _ in range(no_of_sides):
+            tim.right(360 / no_of_sides)
+            tim.forward(100)
+# pentagon = Shapes.polygon(100,5)
+pentagon = Shapes.polygon(5)
+hexagon = Shapes.polygon(6)
+heptagon = Shapes.polygon(7)
+octagon = Shapes.polygon(8)
+nonagon = Shapes.polygon(9)
 
 
 
