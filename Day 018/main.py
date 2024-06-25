f38382309672d5692f8d3cf37fe6920cfda61bc9
Agnes <agnesoserayn@gmail.com>
2024-06-25 12:39:09 +0300

    making turtle to make a square

---
+++ Day 018/main.py
@@ -0,0 +1,26 @@
+from turtle import Turtle, Screen
+# challenge 1 get the turtle to draw a square 100 by 100
+tim = Turtle()
+tim.shape("turtle")
+tim.color("green4")
+
+for _ in range(4):
+    tim.forward(100)
+    tim.right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+my_screen = Screen()
+my_screen.exitonclick()
