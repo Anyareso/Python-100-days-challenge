79a000f05a6e7c7e0bfdfb18210ad36c783d9cde
Agnes <agnesoserayn@gmail.com>
2024-06-26 12:54:25 +0300

    random walk challenge complete

--- Day 018/day-18-start.py
+++ Day 018/day-18-start.py
@@ -1,14 +1,17 @@
 import turtle as t
+from turtle import Screen
 import random
 
 tim = t.Turtle()
-t.colormode(255)
-def random_color():
-    r = random.randint(0, 255)
-    g = random.randint(0, 255)
-    b = random.randint(0, 255)
-    color = (r, g, b)
-    return color
+colors = ["cyan", "olive", "maroon", "dark orchid", "brown", "wheat", "spring green", "dark sea green"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-########### Challenge 5 - Spirograph ########
+for _ in range(200):
+    tim.color(random.choice(colors))
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
 
+my_screen = Screen()
+my_screen.exitonclick()
