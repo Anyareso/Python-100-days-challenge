823477469e70b5f20842c0134a68b122afc1cac7
Agnes <agnesoserayn@gmail.com>
2024-06-26 09:46:33 +0300

    drawing shapes modified calling an instance of the class

--- Day 018/main.py
+++ Day 018/main.py
@@ -2,7 +2,7 @@ from turtle import Turtle, Screen
 # challenge 1 get the turtle to draw a square 100 by 100
 tim = Turtle()
 tim.shape("turtle")
-tim.color("green4")
+# tim.color("green4")
 
 # for _ in range(4):
 #     tim.forward(100)
@@ -15,55 +15,24 @@ tim.color("green4")
 #     tim.pendown()
 
 
-def square():
-    for _ in range(4):
-        tim.right(90)
-        tim.forward(100)
-
-
-def agons():
-    for _ in range(5):
-        tim.right(60)
-        tim.forward(100)
-
-def triangle(side_length):
-    for _ in range(3):
-        tim.right(120)
-        tim.forward(side_length)
-
-
-triangle(100)
-# square()
-# agons()
-
 class Shapes:
     def __init__(self):
-        self.length = 0
-        self.sides = 0
+        pass
 
-    def polygon(no_of_sides):
+    def polygon(self, no_of_sides, color):
         for _ in range(no_of_sides):
+            tim.color(color)
             tim.right(360 / no_of_sides)
             tim.forward(100)
-# pentagon = Shapes.polygon(100,5)
-pentagon = Shapes.polygon(5)
-hexagon = Shapes.polygon(6)
-heptagon = Shapes.polygon(7)
-octagon = Shapes.polygon(8)
-nonagon = Shapes.polygon(9)
 
 
-
-
-
-
-
-
-
-
-
-
-
+shape = Shapes()
+shape.polygon(3, "yellow")
+shape.polygon(5, "orange")
+shape.polygon(6, "blue")
+shape.polygon(7, "pink")
+shape.polygon(8, "purple")
+shape.polygon(9, "brown")
 
 
 my_screen = Screen()
