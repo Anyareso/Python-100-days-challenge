23815dbe1cdb388e63c3162154cc6a99932ecb26
Agnes <agnesoserayn@gmail.com>
2024-06-26 13:01:51 +0300

    applying tuples to the random walk program

--- Day 018/day-18-start.py
+++ Day 018/day-18-start.py
@@ -3,13 +3,23 @@ from turtle import Screen
 import random
 
 tim = t.Turtle()
-colors = ["cyan", "olive", "maroon", "dark orchid", "brown", "wheat", "spring green", "dark sea green"]
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    my_tuple = (r, g, b)
+    return my_tuple
+
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
 
+
 for _ in range(200):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.forward(30)
     tim.setheading(random.choice(directions))
 
