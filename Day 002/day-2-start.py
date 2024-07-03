372d82d4082bd72161b8faf72dbc6757db010685
Agnes <agnesoserayn@gmail.com>
2024-07-03 14:55:12 +0300

    correcting to pep8 code standards

--- Day 002/day-2-start.py
+++ Day 002/day-2-start.py
@@ -1,7 +1,7 @@
-# User shoul input a two digit number e.g, 32
+# User should input a two-digit number e.g, 32
 # Output should be the sum of the number's digits e.g, 5
 
 print("\033c")
 two_digit_number = input("Enter a two digit number")
-sum = int(two_digit_number[0]) + int(two_digit_number[1])
-print(sum)
+summation = int(two_digit_number[0]) + int(two_digit_number[1])
+print(summation)
