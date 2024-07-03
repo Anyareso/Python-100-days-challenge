5ed3004879fd9d962e7ae6330a66ad3a0f2c2807
Agnes <agnesoserayn@gmail.com>
2024-07-03 14:40:50 +0300

    correcting to pep8 code standards

--- Day 001/nameLength.py
+++ Day 001/nameLength.py
@@ -1,6 +1,6 @@
 # Ask user to input their name
 # output the no of characters in their name
 
-name = input("What is your name?")
-length = len (name)
+name = input("What is your name? ")
+length = len(name)
 print(length)
