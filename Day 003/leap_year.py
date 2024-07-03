9db632b4e4ba496b6fc22b78b98e320a47daea7a
Agnes <agnesoserayn@gmail.com>
2024-07-03 15:44:40 +0300

    correcting to pep8 code standards and code refactoring

--- Day 003/leap_year.py
+++ Day 003/leap_year.py
@@ -1,19 +1,15 @@
-# Program that checks if an year is a leap year or not
+# Program that checks if a year is a leap year or not
 # A leap year is divisible by 4 with no remainder
 # Except every year that is evenly divisible by 100 with no remainder
 # Leap year is divisible by 400 with no remainder
-# e.g 2000 / 4 = 500 (leap)
+# e.g. 2000 / 4 = 500 (leap)
 # 2000 / 100 = 20 (not leap)
 # 2000 / 400 = 5 (leap)
 # concluding 2000 is indeed a leap year
 
 year = int(input("Enter any year\n"))
-if year % 4 != 0:
-    print(f"{year} is not a leap year")
-elif year % 100 != 0:
-    print(f"{year} is not a leap year")
-elif year % 400 != 0:
-    print(f"{year} is not a leap year")
-else:
+if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
     print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
 
