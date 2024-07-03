52d0f4f72aa19c193835a888f2163544fb0ae882
Agnes <agnesoserayn@gmail.com>
2024-07-03 16:09:21 +0300

    correcting to pep8 code standards

--- Day 003/treasure_island_end.py
+++ Day 003/treasure_island_end.py
@@ -9,7 +9,7 @@ print('''
 |                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
 |___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
           |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
- _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+ _________|___________| ;`-.o`"=._; ." ` '`."` . "-._ /_______________|_______
 |                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
 |___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
 ____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
@@ -25,7 +25,9 @@ print("Your mission is to find the treasure.")
 
 task1 = input("You're at a crossroad. Where do you want to go? Type 'left' or 'right' ")
 if task1 == "left":
-    print("You are at a lake. There is an island at the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across. ")
+    print(
+        "You are at a lake. There is an island at the middle of the lake. Type 'wait' to wait for a boat."
+        " Type 'swim' to swim across. ")
     task2 = input("What do you choose to do now?")
     if task2 == "wait":
         print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow, one blue.")
@@ -34,7 +36,8 @@ if task1 == "left":
             print("You enter a room with beasts. Game over!")
         elif task3 == "red":
             print("It's a room full of fire. Game Over!")
-        elif task3 == "yellow":print("You've found the treasure. You win!!")
+        elif task3 == "yellow":
+            print("You've found the treasure. You win!!")
         else:
             print("You chose a door that doesn't exist. Game over.")
     else:
