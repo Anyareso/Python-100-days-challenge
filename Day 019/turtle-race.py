de3ca898a6241afe46038b09fb10e14047ceac22
Agnes <agnesoserayn@gmail.com>
2024-07-08 11:31:30 +0300

    creating 6 instances of turtle each with a different color

--- Day 019/turtle-race.py
+++ Day 019/turtle-race.py
@@ -1,12 +1,16 @@
+import random
 from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
-print(user_bet)
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-230, y=-100)
+for i in range(6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[i])
+    tim.goto(x=-230, y=y_positions[i])
 
 screen.exitonclick()
