d604f1a069078c17e31540078c3b7bf3cabe7c96
Agnes <agnesoserayn@gmail.com>
2024-07-08 10:58:55 +0300

    setting turtle shape and turtle not to draw at all

--- Day 019/turtle-race.py
+++ Day 019/turtle-race.py
@@ -5,7 +5,8 @@ screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
 print(user_bet)
 
-tim = Turtle()
+tim = Turtle(shape="turtle")
+tim.penup()
 tim.goto(x=-230, y=-100)
 
 screen.exitonclick()
