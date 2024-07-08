f454187088f80b7040daea0312b3eb460e32142d
Agnes <agnesoserayn@gmail.com>
2024-07-08 10:26:14 +0300

    my challenge solution

---
+++ Day 019/etch-a-sketch-app.py
@@ -0,0 +1,29 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(20)
+
+
+def move_backward():
+    tim.backward(20)
+
+
+def move_clockwise():
+    tim.right(90)
+
+
+def move_anticlockwise():
+    tim.left(90)
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=move_anticlockwise)
+screen.onkey(key="d", fun=move_clockwise)
+
+screen.exitonclick()
