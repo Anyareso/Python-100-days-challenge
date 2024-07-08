674c436d19a264ffd659b1f78f063f589b233def
Agnes <agnesoserayn@gmail.com>
2024-07-08 10:34:36 +0300

    code review

--- Day 004/bankerRoulette.py
+++ Day 004/bankerRoulette.py
@@ -1,7 +1,7 @@
 # This program selects a random name from a list of names.
 # The person selected will pay for everybody's food bill.
 
-# Start by inputing the people present at the table
+# Start by inputting the people present at the table
 # Get the total number of names
 # Pick out a random person from the list by using the random number
 import random
@@ -9,6 +9,7 @@ import random
 names_string = input("Enter the names of people present at the table")
 names = names_string.split(",")
 num_items = len(names)
+print(num_items)
 random_choice = random.randint(0, num_items - 1)
 random_name = names[random_choice]
 
