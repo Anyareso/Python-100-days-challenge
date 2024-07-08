a0bb5f1cbac97f1af97aee450307c08dea0dc179
Agnes <agnesoserayn@gmail.com>
2024-07-08 14:28:30 +0300

    creating a snake

--- Day 020/snake_game/main.py
+++ Day 020/snake_game/main.py
@@ -4,10 +4,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
+positions = [0, -20, -40]
 
-
-
-
+for i in range(0, 3):
+    tim = Turtle(shape="square")
+    tim.color("white")
+    tim.goto(x=positions[i], y=0)
 
 
 
