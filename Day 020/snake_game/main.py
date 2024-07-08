28f068095bbf919653dd16585bce3421545fa7ec
Agnes <agnesoserayn@gmail.com>
2024-07-08 14:30:52 +0300

    using tuples to create a snake

--- Day 020/snake_game/main.py
+++ Day 020/snake_game/main.py
@@ -4,12 +4,12 @@ screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
-positions = [0, -20, -40]
+positions = [(0, 0), (-20, 0), (-40, 0)]
 
-for i in range(0, 3):
+for position in positions:
     tim = Turtle(shape="square")
     tim.color("white")
-    tim.goto(x=positions[i], y=0)
+    tim.goto(position)
 
 
 
