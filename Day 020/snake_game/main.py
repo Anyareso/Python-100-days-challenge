09168c5b692dd90750721ddfed1463a6021d522f
Agnes <agnesoserayn@gmail.com>
2024-07-10 10:33:10 +0300

    calling the class from snake.py

--- Day 020/snake_game/main.py
+++ Day 020/snake_game/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from snake import Snake
 import time
 
 screen = Screen()
@@ -6,26 +7,14 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
-positions = [(0, 0), (-20, 0), (-40, 0)]
-
-segments = []
-
-for position in positions:
-    tim = Turtle(shape="square")
-    tim.color("white")
-    tim.penup()
-    tim.goto(position)
-    segments.append(tim)
+snake = Snake()
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg_num in range(len(segments) - 1, 0, -1):
-        new_x = segments[seg_num - 1].xcor()
-        new_y = segments[seg_num - 1].ycor()
-        segments[seg_num].goto(new_x, new_y)
-    segments[0].forward(20)
+    snake.move()
+
 
 
 
