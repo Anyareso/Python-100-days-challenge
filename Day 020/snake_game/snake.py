fbb43718d3e2f3387f800fe07b7c7dec11f87c7b
Agnes <agnesoserayn@gmail.com>
2024-07-10 10:29:42 +0300

    code refactoring by creating a snake class

---
+++ Day 020/snake_game/snake.py
@@ -0,0 +1,21 @@
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            tim = Turtle(shape="square")
+            tim.color("white")
+            tim.penup()
+            tim.goto(position)
+            self.segments.append(tim)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.segments[0].forward(20)
