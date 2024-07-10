9857574eeeb6aaba97a224301a2486712d46960c
Agnes <agnesoserayn@gmail.com>
2024-07-10 11:00:29 +0300

    enabling control of the snake using keyboard keys

--- Day 020/snake_game/main.py
+++ Day 020/snake_game/main.py
@@ -7,7 +7,15 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
+# Creating the snake
 snake = Snake()
+# Controlling the snake by keyboard keys
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+
 
 game_is_on = True
 while game_is_on:
