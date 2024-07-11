a027e57e9a038131adbcd6c30be0721c1ce6f9d3
Agnes <agnesoserayn@gmail.com>
2024-07-11 14:03:36 +0300

    updating the scoreboard each time snake collides with food

--- Day 020/snake_game/main.py
+++ Day 020/snake_game/main.py
@@ -1,6 +1,7 @@
 from turtle import Screen
 from snake import Snake
 from food import Food
+from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
@@ -14,13 +15,16 @@ snake = Snake()
 
 # Creating the food
 food = Food()
+
+# Establishing the score
+scoreboard = ScoreBoard()
+
 # Controlling the snake by keyboard keys
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
-
 
 game_is_on = True
 while game_is_on:
@@ -31,6 +35,9 @@ while game_is_on:
 #   Detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        scoreboard.increase_score()
+
+
 
 
 
