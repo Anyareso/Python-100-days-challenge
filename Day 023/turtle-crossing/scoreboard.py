7fa520cc9038abdd9a29cf7a35c945ddaf4a7827
Agnes <agnesoserayn@gmail.com>
2024-07-31 15:53:52 +0300

    displaying and updating scoreboard

--- Day 023/turtle-crossing/scoreboard.py
+++ Day 023/turtle-crossing/scoreboard.py
@@ -1,5 +1,27 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
+FONT = ("Courier", 16, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.color("black")
+        self.penup()
+        self.hideturtle()
+        self.level = 1
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.goto(-280, 260)
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def point(self):
+        self.level += 1
+        self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
+
+
