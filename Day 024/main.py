c927735d696726c6a155f6f0c022dc483d59330c
Agnes <agnesoserayn@gmail.com>
2024-08-01 10:51:20 +0300

    mode w deletes and replaces everything in the file

--- Day 024/main.py
+++ Day 024/main.py
@@ -1,3 +1,8 @@
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# reading a file
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# writing a file
+with open("my_file.txt", mode="w") as file:
+    file.write("I cannot be much louder.")
