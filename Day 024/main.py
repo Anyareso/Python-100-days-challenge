c37c917e4453f86a47d4016b53f1a98d3a52d486
Agnes <agnesoserayn@gmail.com>
2024-08-01 10:44:54 +0300

    different way to open a file

--- Day 024/main.py
+++ Day 024/main.py
@@ -1,4 +1,4 @@
-file = open("my_file.txt")
-contents = file.read()
-print(contents)
-file.close()
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+    file.close()
