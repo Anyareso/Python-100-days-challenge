5f5c9f33d4ca7c305a9e25c536d7e2a9fb9bb4a3
Agnes <agnesoserayn@gmail.com>
2024-08-02 10:01:34 +0300

    navigating absolute and relative file paths

--- Day 024/main.py
+++ Day 024/main.py
@@ -1,8 +1,8 @@
 # reading a file
-# with open("my_file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("../../../Desktop/my_file.txt") as file:
+    contents = file.read()
+    print(contents)
 
 # writing a file
-with open("my_file.txt", mode="a") as file:
-    file.write("\nI cannot be much louder.")
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nI cannot be much louder.")
