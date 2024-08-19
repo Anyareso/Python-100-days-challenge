2849371f4e26c25c08b3a8bf50c61d5134f36217
Agnes <agnesoserayn@gmail.com>
2024-08-19 10:19:54 +0300

    applying list comprehension

--- Day 025/us-states-game-start/main.py
+++ Day 025/us-states-game-start/main.py
@@ -21,10 +21,7 @@ while not game_complete:
         continue
     # Exit game
     if answer_state == "Exit":
-        missing_states = []
-        for state in all_states:
-            if state not in correct_answers:
-                missing_states.append(state)
+        missing_states = [state for state in all_states if state not in correct_answers]
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
