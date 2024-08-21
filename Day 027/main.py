8d3a6300cd9cd9a13bc67b0a6d480ab792806518
Agnes <agnesoserayn@gmail.com>
2024-08-21 10:11:45 +0300

    using input entry

--- Day 027/main.py
+++ Day 027/main.py
@@ -6,13 +6,27 @@ window.minsize(width=500, height=300)
 
 # Label
 my_label = Label(text="I am  label", font=("Arial", 24, "bold"))
-my_label.pack(side="left")
+my_label.pack()
 
 my_label["text"] = "New text"
 my_label.config(text="New text")
 
 # Button
-button = Button()
+
+
+def button_clicked():
+    print("I got clicked")
+    new_text = user_input.get()
+    my_label.config(text=new_text)
+
+
+button = Button(text="Click me", command=button_clicked)
+button.pack()
+
+# Entry
+user_input = Entry()
+user_input.pack()
+print(user_input.get())
 
 
 window.mainloop()
