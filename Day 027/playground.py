8fb10656c30235e77f14dd5d2947bfd2bc00014d
Agnes <agnesoserayn@gmail.com>
2024-08-21 09:39:14 +0300

    using kwargs

--- Day 027/playground.py
+++ Day 027/playground.py
@@ -1,8 +1,20 @@
-def add(*args):
-    som = 0
-    for n in args:
-        som += n
-    return som
+# def add(*args):
+#     som = 0
+#     for n in args:
+#         som += n
+#     return som
+#
+#
+# print(add(3, 5, 6))
 
 
-print(add(3, 5, 6))
+class Car:
+    def __init__(self, **kw):
+        self.make = kw.get("make")
+        self.model = kw.get("model")
+        self.color = kw.get("color")
+        self.seats = kw.get("seats")
+
+
+my_car = Car(make="Nissan", model="Skyline")
+print(my_car.model)
