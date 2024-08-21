7642190cf3ba398a8f6d15eff8587ab2b77b69e6
Agnes <agnesoserayn@gmail.com>
2024-08-21 09:48:49 +0300

    different way of importing tkinter

--- Day 027/main.py
+++ Day 027/main.py
@@ -1,11 +1,18 @@
-import tkinter
+from tkinter import *
 
-window = tkinter.Tk()
+window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="I am  label", font=("Arial", 24, "bold"))
-my_label.pack()
+my_label = Label(text="I am  label", font=("Arial", 24, "bold"))
+my_label.pack(side="left")
+
+my_label["text"] = "New text"
+my_label.config(text="New text")
+
+# Button
+button = Button()
+
 
 window.mainloop()
