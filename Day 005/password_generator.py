5df83ed82b4dfcca4ea37fb58b09fac505eb49c8
Agnes <agnesoserayn@gmail.com>
2024-09-16 13:00:27 +0300

    refactoring code for day 28 challenge

--- Day 005/password_generator.py
+++ Day 005/password_generator.py
@@ -8,9 +8,9 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters = int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_letters = random.randint(8, 10)
+nr_symbols = random.randint(2, 4)
+nr_numbers = random.randint(2, 4)
 
 # Eazy Level - Order not randomised:
 # e.g. 4 letter, 2 symbol, 2 number = JduE&!91
@@ -33,13 +33,13 @@ nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 password_list = []
 
-for char in range(1, nr_letters + 1):
+for char in range(nr_letters):
     password_list.append(random.choice(letters))
 
-for char in range(1, nr_symbols + 1):
+for char in range(nr_symbols):
     password_list += random.choice(symbols)
 
-for char in range(1, nr_numbers + 1):
+for char in range(nr_numbers):
     password_list += random.choice(numbers)
 
 random.shuffle(password_list)
