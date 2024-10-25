4332b726afec1ba03b13a15768a249779ff92735
Agnes <agnesoserayn@gmail.com>
2024-10-25 21:43:13 +0300

    creating flashcard words

--- Day 031/flash-card-project-start/main.py
+++ Day 031/flash-card-project-start/main.py
@@ -1,5 +1,17 @@
 from tkinter import *
+import pandas
+import random
 BACKGROUND_COLOR = "#B1DDC6"
+
+data = pandas.read_csv("data/french_words.csv")
+french_words = data.to_dict(orient="records")
+
+
+def next_card():
+    current_card = random.choice(french_words)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+
 
 window = Tk()
 window.title("French Flash-Cards")
@@ -9,16 +21,18 @@ canvas = Canvas(width=800, height=526)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_front_img = PhotoImage(file="images/card_front.png")
 canvas.create_image(400, 263, image=card_front_img)
-canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
-canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 canvas.grid(column=0, row=0, columnspan=2)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-cancel_btn = Button(image=wrong_img, highlightthickness=0)
+cancel_btn = Button(image=wrong_img, highlightthickness=0, command=next_card)
 cancel_btn.grid(column=0, row=1)
 
 right_img = PhotoImage(file="images/right.png")
-cancel_btn = Button(image=right_img, highlightthickness=0)
+cancel_btn = Button(image=right_img, highlightthickness=0, command=next_card)
 cancel_btn.grid(column=1, row=1)
+
+next_card()
 
 window.mainloop()
